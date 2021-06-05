cc941649a2be36107160234df5da8c1caa15c51d
prem-12345 <premkpatil1997@gmail.com>
2021-06-05 08:15:00 +0530

    first commit

--- empwage.sh
+++ empwage.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
 #"DAY 9: EMPLOYEE WAGE COMPUTATION PROBLEM"
+
+#UC_1:CHECK EMPLOYEE PRESENT OR ABSENT
+
+ispresent=1
+isabsent=0
+
+x=$((RANDOM%2))
+
+if [ $x -eq 1 ]
+then
+	echo "Employee is present"
+else
+	echo "Employee is absent"
+fi
