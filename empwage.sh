1b16e7252a8713c7f3ea61952f7a91be0356e8d3
prem-12345 <premkpatil1997@gmail.com>
2021-06-05 08:37:04 +0530

    first commit

--- empwage.sh
+++ empwage.sh
@@ -15,3 +15,24 @@ then
 else
 	echo "Employee is absent"
 fi
+
+#UC_2:CALCULATE DAILY EMPLOYEE WAGE
+
+emphr=8
+salaryperhr=30
+
+x=$((RANDOM%2))
+
+if [ $x -eq 1 ]
+then
+        echo "Employee is present"
+	emphr=8
+else
+        echo "Employee is absent"
+	emphr=0
+fi
+
+Dailywage=$(($emphr*$salaryperhr))
+echo "Dailywage:"$Dailywage
+
+
